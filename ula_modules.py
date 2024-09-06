854d81657b596a8d2d877d1544718f09198539f9
Vitor Kenzo Nishiwaki Fengler <159027253+vitorkenzo299@users.noreply.github.com>
2024-09-06 10:55:00 +0000

    Lab 6

--- ula_modules.py
+++ ula_modules.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 
 """
@@ -11,29 +11,49 @@ from myhdl import *
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
-
+        soma.next = a * (not b) + (not a) * b
+        carry.next = a * b
     return instances()
 
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    s = [Signal(bool(0)) for i in range(3)]
+    
+    haList = [None for i in range(2)]
+
+    haList[0] = halfAdder(a, b, s[0], s[1]) # (2)
+    haList[1] = halfAdder(c, s[0], soma, s[2]) # (3)
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[1] | s[2] # (4)
 
     return instances()
 
 
+
 @block
 def adder2bits(x, y, soma, carry):
+    s1 = Signal(bool(0))
+    
+    half = halfAdder(x[0], y[0], soma[0], s1)
+    full = fullAdder(x[1], y[1], s1, soma[1], carry)
+
     return instances()
 
 
 @block
 def adder(x, y, soma, carry):
-    @always_comb
-    def comb():
-        pass
+    
+    n = len(x)  
+    carry1 = [Signal(bool(0)) for i in range(n)]  
+
+    half = halfAdder(x[0], y[0], soma[0], carry1[0])
+
+    faList = [None for i in range(1, n)]
+    for i in range(1, n):
+        faList[i-1] = fullAdder(x[i], y[i], carry1[i-1], soma[i], carry1[i])
 
     return instances()
+
